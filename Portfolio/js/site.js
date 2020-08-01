d38add1f5fe7db0e1d8cf1a7dff461a3c1f40aad
Julius Shipton <Julius Shipton@DESKTOP-75I7BMU>
2020-07-31 17:20:47 -0700

    Made the navbar appear and dissapear on scroll.  Fixed a bunch of other stuff, got entire site looking good on mobile.

--- Portfolio/js/site.js
+++ Portfolio/js/site.js
@@ -1,5 +1,19 @@
 ﻿
 
+//JS for navbar on scroll
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function () {
+    var currentScrollPos = window.pageYOffset;
+    if (prevScrollpos >= currentScrollPos) {
+        document.getElementById("customNavbar").style.top = "0";
+        document.getElementById("customNavbar").style.zIndex = "1";
+    } else {
+        document.getElementById("customNavbar").style.top = "-150px";
+    }
+    prevScrollpos = currentScrollPos;
+}
+
+
 
 //JS for flyout menu
 var roundButton = document.querySelector("#roundButton");
@@ -8,8 +22,14 @@ roundButton.addEventListener("click", showMenu, false);
 var flyoutMenu = document.querySelector("#flyoutMenu");
 flyoutMenu.addEventListener("click", hideMenu, false);
 
+//fix for info overlapping menu
+var infoDiv = document.querySelector("#info");
+
 function showMenu(e) {
     flyoutMenu.classList.add("show");
+
+    //hide stuff that was overlapping menu
+    infoDiv.style.display = 'none';
 
     document.body.style.overflow = "hidden";
 }
@@ -17,6 +37,9 @@ function showMenu(e) {
 function hideMenu(e) {
     flyoutMenu.classList.remove("show");
     e.stopPropagation();
+
+    //show stuff that was overlapping menu
+    infoDiv.style.display = 'block';
 
     document.body.style.overflow = "auto";
 }
